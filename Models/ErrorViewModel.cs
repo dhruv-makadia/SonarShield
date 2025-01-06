bf6c0cd32fdec0fa4a288797fd51e77d81410c42
sagar.gadhesariya <sagar.gadhesariya@tatvasoft.com>
2025-01-06 11:57:50 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SonarShield.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
